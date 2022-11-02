e37aac4a3932055095d43b4550e71040705704ec
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-11-02 22:10:10 +0100

    printing text

--- c++/src/game.cpp
+++ c++/src/game.cpp
@@ -23,8 +23,9 @@ Game::Game(const std::string & config_path)
 
   unsigned int window_width, window_height;
   config_file >> window_width >> window_height;
-  m_window = new sf::RenderWindow(sf::VideoMode(window_width, window_height), "SFML Works");
+  m_window = std::make_shared<sf::RenderWindow>(sf::VideoMode(window_width, window_height), "SFML Works");
 
+  
   config_file >> object_type;
   if(object_type != "Font")
   {
@@ -34,36 +35,32 @@ Game::Game(const std::string & config_path)
 
   std::string font_filename;
   config_file >> font_filename;
-  sf::Font font;
-  m_font = std::make_shared<sf::Font>(font);
+  m_font = std::make_shared<sf::Font>();
   if(!m_font->loadFromFile(font_filename))
   {
-    std::cerr << "Failed to load font" << std::endl;
+    std::cerr << "Expected font file not found" << std::endl;
     exit(-1);
   }
 
   config_file >> m_font_size;
   uint8_t red, blue, green;
   config_file >> red >> blue >> green;
-  m_font_color = new sf::Color(red, blue, green);
+  m_font_color = std::make_shared<sf::Color>(red, blue, green);
 }
 
 Game::~Game()
 {
-  delete m_window;
-  //delete m_font;
-  delete m_font_color;
 }
 
 void Game::loop()
 {
+  
   sf::Text text;
-  //text.setFont(*m_font);
-  std::cout << m_font;
+  text.setFont(*m_font);
   text.setString("Sample Text");
   text.setCharacterSize(m_font_size);
   text.setFillColor(*m_font_color);
-
+  
   while(m_window->isOpen())
   {
     sf::Event event;
--- c++/src/game.h
+++ c++/src/game.h
@@ -3,10 +3,14 @@
 
 class Game
 {
-  sf::RenderWindow * m_window;
+  std::shared_ptr<sf::RenderWindow> m_window;
   std::shared_ptr<sf::Font> m_font;
   unsigned int m_font_size;
-  sf::Color * m_font_color;
+  std::shared_ptr<sf::Color> m_font_color;
+
+  void loadWindow(std::ifstream & config_file);
+  void loadFont(std::ifstream & config_file);
+  std::string & loadObjectType(std::ifstream & config_file);
 
 public:
 
